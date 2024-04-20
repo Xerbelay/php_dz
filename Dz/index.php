9aa5e21549911f0e41ee1d5e0f135cde609e2696
Xerbelay <148629656+Xerbelay@users.noreply.github.com>
2024-04-20 13:22:21 +0300

    Add files via upload

---
+++ Dz/index.php
@@ -0,0 +1,33 @@
+<?php
+/* 7 */
+/* $a = 'Hello';
+$c = str_split($a, 1);
+for($i=0;$i<strlen($a);$i++){
+    echo $c[$i] . '<br>';
+} */
+
+/* 8 */
+/* $a = 'Hello';
+echo strrev($a); */
+
+/* 9 */
+/* $a = 'jlolj';
+if($a == strrev($a)){
+    echo 'Полиндром';
+}
+else{
+    echo 'Не полиндром';
+} */
+
+/* 10 */
+/* $a = 'Hello my friend';
+echo str_replace(' ', '_', $a); */
+
+/* 11 */
+/* $a = 'Hello my friend';
+$c = explode(' ', $a); 
+$b = array_reverse($c);
+for($i=0;$i<$c;$i++){
+    echo $b[$i] . '<br>';
+} */
+?>
